2674b56d7506f79b768ce2cb480f51d34ef242e6
1876070677 <fixerpremium@gmail.com>
2021-09-24 09:59:04 +0900

    database properties example

---
+++ properties(ex).js
@@ -0,0 +1,12 @@
+var properties = {
+    port : 3000,
+    mysql: {
+        host : "db url",
+        port : "db port",
+        db : "db name",
+        username : "db ID",
+        password : "db password",
+    }
+}
+
+module.exports = properties;
