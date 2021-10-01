bd01fc8df09f7bb75e9e6be180c996db98628877
1876070677 <fixerpremium@gmail.com>
2021-10-01 23:02:37 +0900

    Add Board Info API

--- Server/index.js
+++ Server/index.js
@@ -5,8 +5,10 @@ const app = express();
 app.use(express.json());
 
 var userRouter = require('./routes/users');
+var boardRouter = require('./routes/board');
 
 app.use('/users', userRouter);
+app.use('/board', boardRouter);
   
 // http listen port 생성 서버 실행
 app.listen(3002, () => console.log("Server Start :)"));
--- Server/routes/board.js
+++ Server/routes/board.js
@@ -3,11 +3,11 @@ const getConnection = require('../database/database');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.send("USERS API TEST");
+    res.send("BOARD API TEST");
 })
 
-router.get('/list', function(req, res) {
-    var sql = "SELECT * from accounts;"
+router.get('/list', function(req, res, next) {
+    var sql = "SELECT * from board;"
     getConnection((conn) => {
         conn.query(sql, function(err, results, fields) {
             if(err) {
@@ -19,11 +19,11 @@ router.get('/list', function(req, res) {
         });
         conn.release();
     });
-});
+})
 
-router.get('/list/:id', function(req, res, next) {
-    let user_id = req.params.id;
-    var sql = "SELECT * from accounts WHERE id='" + user_id + "';"
+router.get('/kind/:type', function(req, res, next) {
+    let type = req.params.type;
+    var sql = "SELECT * from board WHERE kind='" + kind + "';"
 
     getConnection((conn) => {
         conn.query(sql, function(err, result, fields) {
@@ -36,12 +36,12 @@ router.get('/list/:id', function(req, res, next) {
         });
         conn.release();
     });
-});
+})
 
 router.post('/add', function(req, res, next) {
     let { id, loc, name, lating } = req.body;
 
-    var sql = "INSERT INTO accounts (id, loc, name, lating) VALUES(?, ?, ?, ?);"
+    var sql = "INSERT INTO board (id, loc, name, lating) VALUES(?, ?, ?, ?);"
     var param = [id, loc, name, lating]
     getConnection((conn) => {
         conn.query(sql, param, function(err, rows, fields) {
@@ -55,22 +55,6 @@ router.post('/add', function(req, res, next) {
         });
         conn.release();
     });
-});
-
-router.delete('/del/:id', function(req, res, next) {
-    let user_id = req.params.id;
-    var sql = "DELETE from accounts where id='" + user_id + "';";
-    getConnection((conn) => {
-        conn.query(sql, function(err, rows, fields) {
-            if(err) {
-                res.json("Failed");
-            }
-            else {
-                res.json("success");
-            }
-        });
-        conn.release();
-    });
-});
+})
 
 module.exports = router;
